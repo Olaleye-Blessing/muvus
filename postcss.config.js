1615cb581767d02b6611ffec850b13b2fbb94d27
Olaleye Blessing <Olayinkablexxy@gmail.com>
2021-09-17 22:03:19 +0100

    fixing scss error

--- postcss.config.js
+++ postcss.config.js
@@ -6,26 +6,36 @@
 //     autoprefixer: {},
 //   },
 // }
-module.exports = {
-    plugins: {
-        "postcss-import": {},
-        autoprefixer: {},
-        tailwindcss: {},
-        "postcss-flexbugs-fixes": {},
-        "postcss-preset-env": {
-            autoprefixer: {
-                flexbox: "no-2009",
-            },
-            stage: 3,
-            features: {
-                "custom-properties": false,
-            },
-        },
-    },
-};
+// module.exports = {
+//     plugins: {
+//         tailwindcss: {},
+//         autoprefixer: {},
+//         "postcss-import": {},
+//         "postcss-flexbugs-fixes": {},
+//         "postcss-preset-env": {
+//             autoprefixer: {
+//                 flexbox: "no-2009",
+//             },
+//             stage: 3,
+//             features: {
+//                 "custom-properties": false,
+//             },
+//         },
+//     },
+// };
 // module.exports = {
 //   plugins: {
 //     tailwindcss: {},
 //     autoprefixer: {},
 //   },
 // }
+// If you want to use other PostCSS plugins, see the following:
+// https://tailwindcss.com/docs/using-with-preprocessors
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+        "postcss-nested": {},
+        "postcss-easy-import": { prefix: "_", extensions: [".css", ".scss"] },
+    },
+};
