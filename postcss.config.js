79390e93ab834b0727b895e4fac4eb290d5ab274
Olaleye Blessing <Olayinkablexxy@gmail.com>
2021-09-18 12:04:04 +0100

    fix sass error

--- postcss.config.js
+++ postcss.config.js
@@ -1,38 +1,6 @@
-// If you want to use other PostCSS plugins, see the following:
-// https://tailwindcss.com/docs/using-with-preprocessors
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// }
-// module.exports = {
-//     plugins: {
-//         tailwindcss: {},
-//         autoprefixer: {},
-//         "postcss-import": {},
-//         "postcss-flexbugs-fixes": {},
-//         "postcss-preset-env": {
-//             autoprefixer: {
-//                 flexbox: "no-2009",
-//             },
-//             stage: 3,
-//             features: {
-//                 "custom-properties": false,
-//             },
-//         },
-//     },
-// };
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// }
-// If you want to use other PostCSS plugins, see the following:
-// https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
     plugins: {
+        "postcss-import": {},
         tailwindcss: {},
         autoprefixer: {},
         "postcss-nested": {},
