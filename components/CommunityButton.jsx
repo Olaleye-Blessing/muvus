4be6bce4e2ea2de04f0250202a3595ad6da3a906
Olaleye Blessing <Olayinkablexxy@gmail.com>
2021-10-03 02:56:19 +0100

    improve community detail/chat
    include other groups that user has joined as sidebar
    add leave button to community page. User returns to general community page upon leave
    add groups' id as part of groups that user has joined

--- components/CommunityButton.jsx
+++ components/CommunityButton.jsx
@@ -1,9 +1,13 @@
+import { useSession } from "next-auth/client";
 import { useRouter } from "next/dist/client/router";
 import { RiGroupFill } from "react-icons/ri";
+import firebase from "firebase/app";
+import { db } from "../firebase";
 import ButtonIcon from "./ButtonIcon";
 
 const CommunityButton = ({ groupName, media_type, id, imgSrc }) => {
     let router = useRouter();
+    let [session, loading] = useSession();
     return (
         <div className="">
             <ButtonIcon
@@ -12,11 +16,53 @@ const CommunityButton = ({ groupName, media_type, id, imgSrc }) => {
                 extraClass="text-red-primary hover:opacity-50"
                 onClick={(e) => {
                     e.preventDefault();
-                    router.push(
-                        `/communities/${media_type}/${id}?imgSrc=${imgSrc}&name=${encodeURIComponent(
-                            groupName
-                        )}`
-                    );
+                    if (!session)
+                        return alert(
+                            "you need to be logged in to join this community"
+                        );
+
+                    db.collection("users")
+                        .where("email", "==", session.user.email)
+                        .get()
+                        .then((querySnapshot) => {
+                            if (querySnapshot.empty)
+                                throw {
+                                    name: "Not Found",
+                                    message:
+                                        "There is no user with the currently loggedin email",
+                                };
+                            querySnapshot.docs[0].ref.set(
+                                {
+                                    communities:
+                                        firebase.firestore.FieldValue.arrayUnion(
+                                            {
+                                                communityId: `${media_type}-${id}`,
+                                                coverPic: imgSrc,
+                                                groupName,
+                                            }
+                                        ),
+                                },
+                                { merge: true }
+                            );
+                        })
+                        .then(() => {
+                            router.push(
+                                `/communities/${media_type}/${id}?imgSrc=${imgSrc}&name=${encodeURIComponent(
+                                    groupName
+                                )}`
+                            );
+                        })
+                        .catch((e) => {
+                            if (e.name === "Not Found") {
+                                return router.push("/");
+                            }
+                        });
+
+                    // router.push(
+                    //     `/communities/${media_type}/${id}?imgSrc=${imgSrc}&name=${encodeURIComponent(
+                    //         groupName
+                    //     )}`
+                    // );
                 }}
             />
         </div>
